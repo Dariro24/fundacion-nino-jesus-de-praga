1f2aa53b51a963cc8ef964f08a530fb7d199d022
Daniel Ricaurte Rodriguez <smdanielrr2015@gmail.com>
2025-06-26 17:41:48 -0500

    versión 0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  }
+  // experimental: {
+  //   viewTransitions: true,
+  // }
 });
